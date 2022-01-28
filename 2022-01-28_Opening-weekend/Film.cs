36343a244e925bf7fe51670e9c6f83a7399c9dbe
szrz_jzsf44 <szarazj.12d@acsjszki.hu>
2022-01-28 13:18:00 +0100

    2,3 feladt

---
+++ 2022-01-28_Opening-weekend/Film.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2022_01_28_Opening_weekend
+{
+    class Film
+    {
+        public string EredetiCim { get; set; }
+        public string MagyarCim { get; set; }
+        public string Bemutato { get; set; }
+        public string Forgalmazo { get; set; }
+        public int Bevetel { get; set; }
+        public int Latogato { get; set; }
+
+        public Film(string sor)
+        {
+            string[] t = sor.Split(';');
+            EredetiCim = t[0];
+            MagyarCim = t[1];
+            Bemutato = t[2];
+            Forgalmazo = t[3];
+            Bevetel = int.Parse(t[4]);
+            Latogato = int.Parse(t[5])
+
+
+
+
+        }
+    }
+}
