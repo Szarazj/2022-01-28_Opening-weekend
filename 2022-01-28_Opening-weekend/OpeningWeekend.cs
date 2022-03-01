fc496594351a9b2d664d7759ea19e2fe9151d5f7
szrz_jzsf44 <szarazj.12d@acsjszki.hu>
2022-03-01 12:36:10 +0100

    Opening-Weeked egész feladat

--- 2022-01-28_Opening-weekend/OpeningWeekend.cs
+++ 2022-01-28_Opening-weekend/OpeningWeekend.cs
@@ -12,12 +12,93 @@ namespace _2022_01_28_Opening_weekend
         static void Main(string[] args)
         {
             List<Film> filmek = new List<Film>();
-            foreach (var sor in File.ReadAllLines("nyitohetvege.txt").Skip(1))
+            File.ReadAllLines("nyitohetvege.txt").Skip(1).ToList().ForEach((s) => {
+                filmek.Add(new Film(s));
+            });
+
+            Console.WriteLine($"3. feladat: Filmek száma az állományban: {filmek.Count} db");
+
+            long uipBevetel = 0;
+            filmek.ForEach((f) => {
+                if (f.forgalmazo == "UIP")
+                {
+                    uipBevetel += f.bevetel;
+                }
+            });
+
+            Console.WriteLine($"4. feladat: UIP Duna Film forgalmazó 1. hetes bevételeinek összege: {uipBevetel.ToString("C2")}");
+
+            Film legtobbLatogato = filmek.OrderBy(f => f.latogatok).Last();
+
+            Console.WriteLine("5. feladat: Legtöbb látogató az első héten:");
+            Console.WriteLine($"\tEredeti cím: {legtobbLatogato.eredetiCim}");
+            Console.WriteLine($"\tMagyar cím: {legtobbLatogato.magyarCim}");
+            Console.WriteLine($"\tForgalmazó: {legtobbLatogato.forgalmazo}");
+            Console.WriteLine($"\tBevétel az első héten: {legtobbLatogato.bevetel.ToString("C2")}");
+            Console.WriteLine($"\tLátogatók száma: {legtobbLatogato.latogatok} fő");
+
+            bool vanFilm = false;
+            filmek.ForEach((f) => {
+                string[] dbMagyarCim = f.magyarCim.Split(' ');
+                string[] dbEredetiCim = f.eredetiCim.Split(' ');
+                if (dbMagyarCim[0].StartsWith("W") && dbMagyarCim[1].StartsWith("W") && dbEredetiCim[0].StartsWith("W") && dbEredetiCim[1].StartsWith("W"))
+                {
+                    vanFilm = true;
+                }
+            });
+
+            Console.WriteLine(vanFilm ? "6. feladat: Volt ilyen film!" : "6. feladat: Nem volt ilyen film!");
+
+            Dictionary<string, int> forgalmazoEsFilmek = new Dictionary<string, int>();
+
+            filmek.ForEach((f) => {
+                if (!forgalmazoEsFilmek.ContainsKey(f.forgalmazo))
+                {
+                    forgalmazoEsFilmek.Add(f.forgalmazo, 1);
+                }
+                else
+                {
+                    forgalmazoEsFilmek[f.forgalmazo]++;
+                }
+            });
+
+            List<string> fileKimenet = new List<string>();
+            fileKimenet.Add("forgalmazo;filmekSzama");
+
+            foreach (var f in forgalmazoEsFilmek)
             {
-                filmek.Add(new Film(sor));
+                if (f.Value > 1)
+                {
+                    fileKimenet.Add($"{f.Key};{f.Value}");
+                }
             }
 
-            Console.WriteLine($"3 feladat: Filmek száma sz állományban: {filmek.Count()} db");
+            File.WriteAllLines("stat.csv", fileKimenet);
+
+            List<DateTime> interComBemutatok = new List<DateTime>();
+
+            filmek.ForEach((f) => {
+                if (f.forgalmazo == "InterCom")
+                {
+                    interComBemutatok.Add(f.bemutato);
+                }
+            });
+
+            int legnagyobbKulonbseg = 0;
+
+            for (int i = 0; i < interComBemutatok.Count; i++)
+            {
+                if (i != 0)
+                {
+                    int kulonbsegSzamitott = (interComBemutatok[i] - interComBemutatok[i - 1]).Days;
+                    if (kulonbsegSzamitott > legnagyobbKulonbseg)
+                    {
+                        legnagyobbKulonbseg = kulonbsegSzamitott;
+                    }
+                }
+            }
+
+            Console.WriteLine($"8. feladat: A leghosszabb időszak két InterCom-os bemutató között: {legnagyobbKulonbseg} nap");
 
             Console.ReadKey();
         }
